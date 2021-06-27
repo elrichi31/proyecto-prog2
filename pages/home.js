b7caa637218e0ff7fa3f03a06774929984115e80
elrichi31 <nicolawsmoina@gmail.com>
2021-06-26 21:54:08 -0500

    Form

--- pages/home.js
+++ pages/home.js
@@ -1,16 +1,100 @@
 import Button from "./Components/Button";
+import styles from "../styles/Home2.module.css";
 export default function Home() {
   return (
-    <div>
-      <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+    <form>
+      <h1>Create user</h1>
+      <div class="row g-3">
+        <div class="col-sm-4">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Nombre"
+            aria-label="Nombre"
+          />
+        </div>
+        <div class="col-sm-4">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Apellido"
+            aria-label="Apellido"
+          />
+        </div>
+        <div class="col-sm-3">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="CI / Pasaporte"
+            aria-label="CI"
+          />
+        </div>
+        <div class="col-sm">
+          <input
+            type="number"
+            class="form-control"
+            placeholder="Edad"
+            aria-label="Edad"
+          />
+        </div>
+        <div class="col-sm-3">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Estado civil"
+            aria-label="Estado civil"
+          />
+        </div>
+        <div class="col-sm-3">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Nacionalidad"
+            aria-label="Nacionalidad"
+          />
+        </div>
+        <div class="col-sm-3">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Profesión"
+            aria-label="Profesión"
+          />
+        </div>
+        <div class="col-sm-3">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Celular"
+            aria-label="Celular"
+          />
+        </div>
+        <div class="col-12">
+          <label for="inputAddress" class="form-label">
+            Dirección
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            id="inputAddress"
+            placeholder="1234 Main St"
+          />
+        </div>
+        <div class="col-12">
+          <label for="inputAddress2" class="form-label">
+            Dirección 2
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            id="inputAddress2"
+            placeholder="Apartment, studio, or floor"
+          />
+        </div>
+        <div class="col-3">
+          <button type="submit" class="btn btn-primary">Submit</button> 
+        </div>
       </div>
-      <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-      </div>
-      <Button></Button>
-    </div>
+    </form>
   );
 }
